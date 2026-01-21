d6dcec753a96d8ee689a3223e2ab7603718d85a4
Andrew McSparron <Amcsparron@albanyny.gov>
2026-01-21 10:16:45 -0500

    Enhance directory template with SVG-based "Go Back" button and streamline template logic
    - Replaced text-based "Go Back" link with an SVG-based button for improved UI.
    - Updated `HTMLTemplateBuilder` to dynamically load and include SVG assets.
    - Added utility methods for validating and resolving asset paths.
    - Introduced multiple SVG assets for future customization.

--- EasyHTTPServerAJM/CustomHandlers/pretty_dir_handler.py
+++ EasyHTTPServerAJM/CustomHandlers/pretty_dir_handler.py
@@ -8,18 +8,72 @@ import socket
 from typing import Union, Optional
 
 
-class HTMLTemplateBuilder:
-    DEFAULT_HTML_TEMPLATE_PATH = Path('../Misc_Project_Files/templates/directory_page_template.html').resolve()
+class _AssetHelper:
+    ...
 
-    def __init__(self, html_template_path: Optional[Union[str, Path]] = None):
+
+class HTMLTemplateBuilder:
+    DEFAULT_ASSETS_PATH = Path('../Misc_Project_Files/assets').resolve()
+    DEFAULT_TEMPLATES_PATH = Path('../Misc_Project_Files/templates').resolve()
+    DEFAULT_HTML_TEMPLATE_PATH = Path(DEFAULT_TEMPLATES_PATH, 'directory_page_template.html').resolve()
+    DEFAULT_BACK_SVG_PATH = Path(DEFAULT_ASSETS_PATH, 'back.svg').resolve()
+
+    def __init__(self, html_template_path: Optional[Union[str, Path]] = None, **kwargs):
+        self._templates_path = None
         self._html_template_path = None
+        self._assets_path = None
+        self._back_svg_path = None
 
         self.html_template_path = (html_template_path if html_template_path is not None
                                    else self.__class__.DEFAULT_HTML_TEMPLATE_PATH)
+        self.assets_path = kwargs.get('assets_path', self.__class__.DEFAULT_ASSETS_PATH)
+        self.back_svg_path = kwargs.get('back_svg_path', self.__class__.DEFAULT_BACK_SVG_PATH)
+        self.back_svg = Path(self.back_svg_path).read_text(encoding='utf-8')
 
+        # enc = encoding for the HTML page
         self.enc = None
         self.title = None
         self.displaypath = None
+
+    @staticmethod
+    def _resolve_path(candidate_path: Union[str, Path]) -> Path:
+        if isinstance(candidate_path, str):
+            candidate_path = Path(candidate_path).resolve()
+        elif isinstance(candidate_path, Path):
+            candidate_path = candidate_path.resolve()
+        else:
+            raise ValueError(f"{candidate_path} is not a valid path")
+        return candidate_path
+
+    def _is_resolved_to_dir(self, candidate_path: Union[str, Path]) -> bool:
+        candidate_path = self._resolve_path(candidate_path)
+        if candidate_path.is_dir():
+            return True
+        else:
+            raise ValueError(f"{candidate_path} is not a valid directory")
+
+    def _is_resolved_to_html(self, candidate_path: Union[str, Path]) -> bool:
+        candidate_path = self._resolve_path(candidate_path)
+        if candidate_path.is_file() and candidate_path.suffix == '.html':
+            return True
+        else:
+            raise ValueError(f"{candidate_path} is not a valid html file")
+
+    def _is_resolved_to_svg(self, candidate_path: Union[str, Path]) -> bool:
+        candidate_path = self._resolve_path(candidate_path)
+        if candidate_path.is_file() and candidate_path.suffix == '.svg':
+            return True
+        else:
+            raise ValueError(f"{candidate_path} is not a valid svg file")
+
+    @property
+    def templates_path(self):
+        return self._templates_path
+
+    @templates_path.setter
+    def templates_path(self, value: Union[str, Path]):
+        if self._is_resolved_to_dir(value):
+            self._templates_path = self._resolve_path(value)
 
     @property
     def html_template_path(self):
@@ -27,7 +81,26 @@ class HTMLTemplateBuilder:
 
     @html_template_path.setter
     def html_template_path(self, value: Union[str, Path]):
-        self._html_template_path = Path(value).resolve()
+        if self._is_resolved_to_html(value):
+            self._html_template_path = self._resolve_path(value)
+
+    @property
+    def assets_path(self):
+        return self._assets_path
+
+    @assets_path.setter
+    def assets_path(self, value: Union[str, Path]):
+        if self._is_resolved_to_dir(value):
+            self._assets_path = self._resolve_path(value)
+
+    @property
+    def back_svg_path(self):
+        return self._back_svg_path
+
+    @back_svg_path.setter
+    def back_svg_path(self, value: Union[str, Path]):
+        if self._is_resolved_to_svg(value):
+            self._back_svg_path = self._resolve_path(value)
 
     def _build_directory_rows(self, entries, path):
         table_rows = []
@@ -47,7 +120,8 @@ class HTMLTemplateBuilder:
         full_context = {'title': self.title,
                         'enc': self.enc,
                         'parent_dir_link': parent_dir_link,
-                        'rows': rows}
+                        'rows': rows,
+                        'back_svg': self.back_svg}
 
         return {**full_context, **(add_to_context or {})}
 
@@ -58,6 +132,8 @@ class HTMLTemplateBuilder:
     @staticmethod
     def _process_directory_entry(path, name):
         fullname = os.path.join(path, name)
+        # TODO: add stats
+        # print(os.stat(fullname))
         display = name + ("/" if os.path.isdir(fullname) else "")
         link = escape(display)
         return f"<tr><td><a href='{link}'>{display}</a></td></tr>"
